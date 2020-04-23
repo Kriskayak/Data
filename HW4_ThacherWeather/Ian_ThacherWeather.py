2c2f0e6a5d39beafb2b8313661b78e67ea95ee15
Jon Swift <jswift@thacher.org>
2020-04-23 16:02:28 -0700

    Graded Ian HW4

--- HW4_ThacherWeather/Ian_ThacherWeather.py
+++ HW4_ThacherWeather/Ian_ThacherWeather.py
@@ -5,6 +5,9 @@
 
 # Note: 2015 Data starts mid-April
 
+#--------------------------------------------------
+# js - There are a lot of extraneous comments in
+# here, like the Input numbers from your notebook
 
 # In[2]:
 
@@ -34,6 +37,9 @@ data = pd.read_csv('WeatherLink_Data_2015.txt',sep='\t',
 
 # In[10]:
 
+#--------------------------------------------------
+# js - Do you really need this as you have specified
+# all the header keys explicitly?
 data.keys()
 
 # In[13]:
@@ -42,21 +48,27 @@ data.keys()
 # Get just the values using this method
 temp = data['Temp Out'].values
 
+#--------------------------------------------------
+# js - These are already floats.
 temp.astype('float')
 # In[17]:
 
+#--------------------------------------------------
+# js - Remenant comment. Make this code your own!
 # You can do fun and useful appending of dataframe columns to make new dataframes
 dtDF = data['Date']+' '+data['Time']
 
 # In[18]:
-
+#--------------------------------------------------
+# js - Ditto above...
 # datetime objects are something that we will work with a lot in this class. More to come
 dt = pd.to_datetime(dtDF).values
 
 
 # In[21]:
 
-
+#--------------------------------------------------
+# js - This is a remnant as well and not needed.
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 fig,ax = plt.subplots()
@@ -68,7 +80,8 @@ plt.ylabel('Temperature')
 
 # In[22]:
 
-
+#--------------------------------------------------
+# js - This is a remnant as well and not needed.
 # looks like all the data are there, but you can't see any detail
 fig,ax = plt.subplots()
 plt.plot_date(dt,temp,'k-')
@@ -119,14 +132,18 @@ for i in np.arange(31)+1:
 
 # In[29]:
 
-
+#--------------------------------------------------
+# js - This does not work as there are "NaN"s in
+# the data arrays.
 maxhist = plt.hist(maxs,alpha=0.5)
 minhist = plt.hist(mins,alpha=0.5)
 
 
 # In[30]:
 
-
+#--------------------------------------------------
+# js - this is well done!, but what about a y axis
+# label?
 maxhist = plt.hist(maxs,alpha=0.5,bins=np.arange(50)*3,label='Daily Highs')
 minhist = plt.hist(mins,alpha=0.5,bins=np.arange(50)*3,label='Daily Lows')
 plt.xlim(30,100)
@@ -135,5 +152,22 @@ plt.legend()
 plt.title('Temperature Highs/Lows May 2015')
 
 
-
 # %%
+'''
+js comments
+-----------
+ - Commenting is sparse and there is a paucity of useful comments
+
+ - Could have looked closer at the code to understand what it is
+   doing.
+
+ - A lot of extraneous code that is not working toward the primary goal
+   of the assignment
+
+ - Bug in the plotting of the data due to NaNs
+
+ - Final plot looks good!
+
+ - 12/15
+
+'''
