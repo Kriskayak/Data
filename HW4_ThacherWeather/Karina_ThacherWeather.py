66d124ea89677948ded8b82b3978831f406d0ca5
Jon Swift <jswift@thacher.org>
2020-04-23 15:49:12 -0700

    Graded Karina HW4

--- HW4_ThacherWeather/Karina_ThacherWeather.py
+++ HW4_ThacherWeather/Karina_ThacherWeather.py
@@ -9,19 +9,29 @@ and states whether they are significantly different (p value greater than 0.05).
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import scipy
-from scipy import stats
+#--------------------------------------------------
+# js - since you are only using the t-test from
+# scipy, you probably should import it this way...
+from scipy.stats import ttest_ind
+
+#from scipy import stats
+#import scipy
 
 ### Part 1: Graphing the highs and lows ###
 
 # Look at the data set to see how to clean it up
-data = pd.read_csv('/home/karina/Desktop/Data_Science_Spring_2020/datasci2020/2_DataWrangling/ThacherWeather/WeatherLink_Data_2015.txt',
-                  sep='\t')
+
+#--------------------------------------------------
+# js - Changing path to work locally add path
+# variable to file name if needed
+path = '/home/karina/Desktop/Data_Science_Spring_2020/datasci2020/2_DataWrangling/ThacherWeather/'
+data = pd.read_csv('WeatherLink_Data_2015.txt',sep='\t')
 
 # Clean up the needed columns, relabel data
-data = pd.read_csv('/home/karina/Desktop/Data_Science_Spring_2020/datasci2020/2_DataWrangling/ThacherWeather/WeatherLink_Data_2015.txt',
-                  sep='\t', usecols=[0,1,2], skiprows=[0,1], header=0, names=['Date','Time','Temp Out'], na_values='--') 
-
+data = pd.read_csv('WeatherLink_Data_2015.txt',sep='\t', usecols=[0,1,2], skiprows=[0,1],
+                   header=0, names=['Date','Time','Temp Out'], na_values='--') 
+#--------------------------------------------------
+# js - Getting error
 # Get the temperature values as a nparray
 temp = data['Temp Out'].values
 
@@ -47,28 +57,50 @@ maxs = maxs.astype('float')
 mins = mins.astype('float')
 
 # Plot histogram
+#--------------------------------------------------
+# js - best to get used to the interactive plot
+# feature. This way you don't usurp your prompt
+# every time you make a plot.
+plt.ion()
+plt.figure()
+plt.clf()
 maxhist = plt.hist(maxs,alpha=0.5,bins=np.arange(50)*3,label='Daily Highs')
 minhist = plt.hist(mins,alpha=0.5,bins=np.arange(50)*3,label='Daily Lows')
 plt.xlim(30,100)
 plt.xlabel('Outside Temperature ($^\circ\!$F)')
+#--------------------------------------------------
+# js - y axis label?
 plt.legend()
 plt.title('Temperature Highs/Lows January 2014')
-plt.show()
+#plt.show()
+
+
+
+
+#--------------------------------------------------
+# js - create more of a break here for flow
+
+
 
 
 ### Part 2: Taking two data sets and comparing to see if the temps are significantly different ###
 # Note, I reload the same data as in Part 1 just for the sake of clarity, it can just be reused, might have to rename some stuff tho
 
 # Load the two data sets, relabeling and cleaning up the dataframes
-data1 = pd.read_csv('/home/karina/Desktop/Data_Science_Spring_2020/datasci2020/2_DataWrangling/ThacherWeather/WeatherLink_Data_2015.txt',
+data1 = pd.read_csv('WeatherLink_Data_2015.txt',
                   sep='\t', usecols=[0,1,2], skiprows=[0,1], header=0, names=['Date','Time','Temp Out'], na_values='--')
 
-data2 = pd.read_csv('/home/karina/Desktop/Data_Science_Spring_2020/datasci2020/2_DataWrangling/ThacherWeather/WS_data_2014.txt',sep='\t',
+data2 = pd.read_csv('WS_data_2014.txt',sep='\t',
                         usecols=[0,2,3], skiprows=[0,1], header=0, names=['Date', 'Time', 'Temp Out'], na_values = '--')
 
 # Pull out the temperatures
 temp1 = data1['Temp Out'].values
 temp2 = data2['Temp Out'].values
+
+#--------------------------------------------------
+# js - the next two tasks could easily be
+# compressed into a for loop or modular functions
+
 
 # Index the temperatures with datetimes
 dtDF1 = data1['Date']+' '+data1['Time']
@@ -110,17 +142,44 @@ maxs2 = maxs2.astype('float')
 mins2 = mins2.astype('float')
 
 # Compare the maxes
-statistic_max, pvalue_max = scipy.stats.ttest_ind(maxs1, maxs2)
+statistic_max, pvalue_max = ttest_ind(maxs1, maxs2)
 
-if pvalue < 0.05:
+#--------------------------------------------------
+# js - I think you got this logic backwards. If the
+# p value is small it means the values ARE
+# significantly different. Also, your variable
+# "pvalue" is not defined. I fixed... 
+
+if pvalue_max < 0.05:
     print("The mean high is not significantly different between years")
 else:
     print("The mean highs are significantly different")
 
 # Compare the mins
-statistic_min, pvalue_min = scipy.stats.ttest_ind(mins1, mins2)
+statistic_min, pvalue_min = ttest_ind(mins1, mins2)
 
-if pvalue < 0.05:
+if pvalue_min < 0.05:
     print("The mean low is not significantly different between years")
 else:
     print("The mean lows are significantly different")
+
+
+#--------------------------------------------------
+# js - one logical thing to do here would be to
+# visualize the two datasets
+
+
+'''
+--------------------------------------------------
+js comments
+-----------
+ - Well commented!
+
+ - Good variable naming
+
+ - Aside from the flipped logic and bug with your p-value output
+   this was a solid piece of code. 
+
+ - 15/15
+
+'''
