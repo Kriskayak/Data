09debe96afaecb849dd8f85db14f8bdee47967eb
“Jennifer” <jyim@thacher.orggit clone git@gitlab.com:jonswift/datasci2020.gitgit clone jyim@gitlab.com:jonswift/datasci2020.gitgit config --global user.email jyim@thacher.org>
2020-05-22 02:23:31 +0900

    minor edits

--- Projects/Jennifer/CoinToss_HW.py
+++ Projects/Jennifer/CoinToss_HW.py
@@ -15,7 +15,7 @@ import numpy as np
 
 df = pd.read_csv("/Users/jennifer/Jen_Data_Science/Coin Toss Experiment_ DataSci 2020 - Sheet1.csv")
 
-def likelyhood(data,p):
+def likelihood(data,p):
        
     likely = 1
        
@@ -37,7 +37,7 @@ for name in df.columns[1:]:
 
     for i in np.arange(0, 1, 0.01):
         xValues.append(i)
-        yValues.append(likelyhood(df[name],i))
+        yValues.append(likelihood(df[name],i))
     
     plt.subplot(plotIndex)
     plotIndex += 1
@@ -45,5 +45,5 @@ for name in df.columns[1:]:
     plt.plot(xValues,yValues)
     plt.title(name)
     plt.xlabel('p')
-    plt.ylabel('likelyhood')
+    plt.ylabel('likelihood')
     plt.subplots_adjust(top=0.92, bottom=0.08, left=0.1, right=0.95, hspace=0.5, wspace=0.5)
