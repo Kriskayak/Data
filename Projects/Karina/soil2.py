9e7c128cb0a885e4e66a9c63538af764c0944b1c
karina andersen <kandersen@thacher.org>
2020-05-26 08:00:26 -0700

    re-did everything with new data

---
+++ Projects/Karina/soil2.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import gaussian_kde
+import statsmodels
+from statsmodels.stats.diagnostic import normal_ad
+
+
+
+#--------------------------------------------------
+# js - I've suggested a bit of fomatting...
+data = pd.read_csv('Soil_Microbial_Biomass_C_N_P_spatial.csv',encoding='latin1',
+                   usecols=[0,1,2,3,4,5,8,9,10,11,12,13,14,15,16,17,18],
+                    skiprows=[0], header=0,
+                   names=['Country','Biome','Latitude','Longitude','Elevation',
+                          'Vegetation','Soil_microbial_biomass_carbon',
+                          'Soil_microbial_biomass_nitrogen','Soil_microbial_biomass_phosphorus',
+                          'Soil_organic_carbon','Total_nitrogen','Total_organic_phosphorus',
+                          'pH','Date','Upper_depth','Lower_depth','Depth'],
+                   na_values='--')
+
+#data = data.fillna(0)
+
+categories = ['Soil_microbial_biomass_carbon', 'Soil_microbial_biomass_nitrogen','Soil_microbial_biomass_phosphorus','Total_nitrogen','Total_organic_phosphorus']
+# Create a for loop to do this for all the categories
+# Not sure of the most effective way to do that b/c each category has a different optimal range of values for the query command below and dif bins
+# Maybe make a dictionary with category and range? and loop using that
+
+
+microbial_carbon = data['Soil_microbial_biomass_carbon'] # Extract the category and get rid of na values
+microbial_carbon = microbial_carbon.dropna()
+
+
+plt.ion() # Setting up interactive plot
+plt.figure()
+plt.clf() 
+
+
+hist = plt.hist(microbial_carbon, alpha=0.5,density=True,bins=np.arange(50)*6, label='Soil Microbial Carbon') # Make hist of data
+plt.xlim(0,300)
+plt.xlabel('Soil Microbial Carbon (mmol/kg)')
+plt.ylabel('Frequency')
+plt.title('Soil Microbial Carbon')
+plt.savefig('Soil_Microbial_Carbon_Plot.png')
+
+
+microbial_carbon = microbial_carbon.to_frame() # Get rid of the outliers and get std/mean
+microbial_carbon = microbial_carbon.query('0 <= Soil_microbial_biomass_carbon <= 300')
+microbial_carbon = microbial_carbon.to_numpy()
+std = np.std(microbial_carbon)
+mean = np.mean(microbial_carbon)
+print(std, mean)
+
+
+def gauss(sig=1,x0=0): # Gaussian function
+    x = np.linspace(x0-10*sig,x0+10*sig,1000)
+    y = 1.0/(np.sqrt(2*np.pi)*sig)*np.exp(-(x-x0)**2/(2*sig**2))
+    return x,y
+
+x,y = gauss(sig=std,x0=mean) # Obviously not a Gaussian, I think it could be lognormal but I don't know how to fit that
+plt.plot(x,y,'r--')
+
+ad, p = normal_ad(microbial_carbon) # A-D test anyways, it gets 0.0 lol
+print(p)
+
+plt.ion()
+plt.figure()
+
+kde = gaussian_kde(microbial_carbon)
+xvals = np.linspace(0, 300, 10000)
+
+plt.plot(xvals,kde.pdf(xvals),'r--')
+plt.xlim(0,300)
+plt.hist(microbial_carbon,bins=np.arange(50)*6,density=True)
+plt.ylabel('$p(microbial carbon)',fontsize=18)
+plt.xlabel('microbial_carbon',fontsize=18)
+plt.savefig('Microbial_Carbon_KDE.png')
--- Projects/Karina/soil_data.py
+++ Projects/Karina/soil_data.py
@@ -1,3 +1,4 @@
+# Imports 
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -5,73 +6,82 @@ from scipy.stats import gaussian_kde
 import statsmodels
 from statsmodels.stats.diagnostic import normal_ad
 
-
-
-#--------------------------------------------------
-# js - I've suggested a bit of fomatting...
-data = pd.read_csv('Soil_Microbial_Biomass_C_N_P_spatial.csv',encoding='latin1',
-                   usecols=[0,1,2,3,4,5,8,9,10,11,12,13,14,15,16,17,18],
+# Reducing and cleaning up the data
+data = pd.read_csv('co-emissions-per-capita-vs-gdp-per-capita-international-.csv',
+                    usecols=[0,2,3,4,5],
                     skiprows=[0], header=0,
-                   names=['Country','Biome','Latitude','Longitude','Elevation',
-                          'Vegetation','Soil_microbial_biomass_carbon',
-                          'Soil_microbial_biomass_nitrogen','Soil_microbial_biomass_phosphorus',
-                          'Soil_organic_carbon','Total_nitrogen','Total_organic_phosphorus',
-                          'pH','Date','Upper_depth','Lower_depth','Depth'],
-                   na_values='--')
+                    names=['Country', 'Year', 'CO2_emissions', 'GDP', 'Total_population'],
+                    na_values='--')
 
-#data = data.fillna(0)
+data_2015 = data.loc[data['Year'] == '2015']
+data_2015 = data_2015.dropna()
+data_2015 = data_2015.reset_index()
+del data_2015['index']
+data_2015_simple = data_2015[['Country','CO2_emissions','GDP','Total_population']]
+N = data_2015_simple.shape[0]
 
-categories = ['Soil_microbial_biomass_carbon', 'Soil_microbial_biomass_nitrogen','Soil_microbial_biomass_phosphorus','Total_nitrogen','Total_organic_phosphorus']
-# Create a for loop to do this for all the categories
-# Not sure of the most effective way to do that b/c each category has a different optimal range of values for the query command below and dif bins
-# Maybe make a dictionary with category and range? and loop using that
+# Preliminary scatter plot of data - Dif colors for each point, size of point corresponds to population
+np.random.seed(521)
+colors = np.random.rand(N)
+size = np.sqrt(np.sqrt(data_2015_simple['Total_population']))
 
+x = list(data_2015_simple.GDP)
+y = list(data_2015_simple.CO2_emissions)
+plt.xlabel('GDP per Capita')
+plt.ylabel('CO2 Emissions per Capita')
+plt.title('CO2 Emissions Based on GDP')
+plt.xscale('log')
+#plt.annotate(list(data_2015_simple.Country), ((data_2015_simple.GDP), (data_2015_simple.CO2_emissions)))
+plt.scatter(x, y, s=size, c=colors, alpha=0.5)
+plt.savefig('Scatter_CO2_Emissions_vs_GDP')
 
-microbial_carbon = data['Soil_microbial_biomass_carbon'] # Extract the category and get rid of na values
-microbial_carbon = microbial_carbon.dropna()
+# Preliminary fitting a line 
+fit = np.polyfit(x, y, 1, full=False,cov=True )
+fitparams = fit[0]
+slope = fitparams[0]
+intercept = fitparams[1]
 
+cov = fit[1]
+param_error = np.sqrt(np.diagonal(cov))
+slope_error = param_error[0]
+intercept_error = param_error[1]
 
-plt.ion() # Setting up interactive plot
-plt.figure()
-plt.clf() 
+xfit = np.linspace(plt.xlim()[0],plt.xlim()[1],100)
+yfit = intercept + slope*xfit
+plt.plot(xfit,yfit,'r--') # Looks pretty good ngl
+plt.savefig('Preliminary_Line_Fit')
 
-
-hist = plt.hist(microbial_carbon, alpha=0.5,density=True,bins=np.arange(50)*6, label='Soil Microbial Carbon') # Make hist of data
-plt.xlim(0,300)
-plt.xlabel('Soil Microbial Carbon (mmol/kg)')
-plt.ylabel('Frequency')
-plt.title('Soil Microbial Carbon')
-plt.savefig('Soil_Microbial_Carbon_Plot.png')
-
-
-microbial_carbon = microbial_carbon.to_frame() # Get rid of the outliers and get std/mean
-microbial_carbon = microbial_carbon.query('0 <= Soil_microbial_biomass_carbon <= 300')
-microbial_carbon = microbial_carbon.to_numpy()
-std = np.std(microbial_carbon)
-mean = np.mean(microbial_carbon)
+# Basix data values
+std = np.std(data_2015_simple.CO2_emissions)
+mean = np.mean(data_2015_simple.CO2_emissions)
 print(std, mean)
 
-
-def gauss(sig=1,x0=0): # Gaussian function
+# Seeing how closely related the data is to a Gaussian
+def gauss(sig=1,x0=0):
     x = np.linspace(x0-10*sig,x0+10*sig,1000)
     y = 1.0/(np.sqrt(2*np.pi)*sig)*np.exp(-(x-x0)**2/(2*sig**2))
     return x,y
 
-x,y = gauss(sig=std,x0=mean) # Obviously not a Gaussian, I think it could be lognormal but I don't know how to fit that
-plt.plot(x,y,'r--')
+# Histogram of CO2 Emissions
+hist = plt.hist(data_2015_simple.CO2_emissions,bins=50,density=True)
 
-ad, p = normal_ad(microbial_carbon) # A-D test anyways, it gets 0.0 lol
-print(p)
+x_gauss,y_gauss = gauss(sig=std,x0=mean)
+plt.plot(x_gauss,y_gauss,'r--')
+plt.savefig('Attempted_Gaussian_Fit')
 
-plt.ion()
-plt.figure()
+ad, p = normal_ad(data_2015_simple.CO2_emissions) # A-D test
+print(p) # Basically 0
 
-kde = gaussian_kde(microbial_carbon)
-xvals = np.linspace(0, 300, 10000)
+# KDE Estimation
+kde = gaussian_kde(data_2015_simple.CO2_emissions)
+xvals = np.linspace(0, 50, 10000)
 
 plt.plot(xvals,kde.pdf(xvals),'r--')
-plt.xlim(0,300)
-plt.hist(microbial_carbon,bins=np.arange(50)*6,density=True)
-plt.ylabel('$p(microbial carbon)',fontsize=18)
-plt.xlabel('microbial_carbon',fontsize=18)
-plt.savefig('Microbial_Carbon_KDE.png')
+plt.xlim(0,50)
+plt.hist(data_2015_simple.CO2_emissions,bins=np.arange(50),density=True)
+plt.ylabel('Frequency',fontsize=12)
+plt.xlabel('CO2 Emissions Per Capita',fontsize=12)
+plt.title('KDE Plot')
+plt.savefig('CO2_Emissions_KDE.png')
+
+# Log Likelihood for Linear Fit
